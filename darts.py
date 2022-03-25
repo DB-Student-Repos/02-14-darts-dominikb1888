fc6c761de8e26ab1721c0b18df842aeb5e277c70
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-03-25 09:20:37 +0100

    pythagorean triple approach

--- darts.py
+++ darts.py
@@ -1,2 +1,13 @@
+from math import sqrt
+
+
 def score(x, y):
-    pass
+    r = sqrt(x**2 + y**2)
+
+    if r <= 1:
+        return 10
+    if 1 < r <= 5:
+        return 5
+    if 5 < r <= 10:
+        return 1
+    return 0

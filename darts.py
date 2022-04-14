d32e2bc68db70b8f6254d85028f83966a1459d1e
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-14 22:25:01 +0200

    cleanup and change of test cases, code passes all test. Logic can be cleaned... double assessment of 0 and 10...

--- darts.py
+++ darts.py
@@ -1,22 +1,31 @@
 from math import sqrt
+from collections import OrderedDict
 
 
 def score(x, y, circles):
     """Calculates score for location of dart on dartboard"""
     r = sqrt(x**2 + y**2)
-    scoring = dict(zip(circles, list(reversed(circles))))
-    return calc_score(r, scoring)
+    return {
+        (r == 0): 10,
+        (0 < r <= 10): calc_score(r, circles),
+        (r > 10): 0,
+    }.get(True)
 
 
-def calc_score(r, scoring):
+def calc_score(r, circles):
     # Potential change of the api to get upper bound from data or to ask for additional variable with
-    if r in range(10):
-        scr = list(scoring.keys())
-        if scr[0] != 0:
-            scr.insert(0, 0)
-        if scr[-1] != 10:
-            scr.append(10)
-        for i in range(len(scr) - 1):
-            if int(r) in range(scr[i], scr[i + 1]):
-                return scoring[scr[i + 1]]
-    return 0
+    scoring = create_score_dict(circles)
+    scrlist = list(scoring.keys())
+    print(scoring, scrlist)
+    for i in range(len(scrlist) - 1):
+        if scrlist[i] < r <= scrlist[i + 1]:
+            return scoring[scrlist[i]]
+
+
+def create_score_dict(circles):
+    circles = sorted(circles)
+    if circles[0] != 0:
+        circles.insert(0, 0)
+    if circles[-1] != 10:
+        circles.append(10)
+    return OrderedDict(zip(circles, list(reversed(circles))))
--- darts_test.py
+++ darts_test.py
@@ -5,47 +5,48 @@ from darts import (
 )
 
 # Tests adapted from `problem-specifications//canonical-data.json`
+scrbrd = [1, 5]
 
 
 class DartsTest(unittest.TestCase):
     def test_missed_target(self):
-        self.assertEqual(score(-9, 9), 0)
+        self.assertEqual(score(-9, 9, scrbrd), 0)
 
     def test_on_the_outer_circle(self):
-        self.assertEqual(score(0, 10), 1)
+        self.assertEqual(score(0, 10, scrbrd), 1)
 
     def test_on_the_middle_circle(self):
-        self.assertEqual(score(-5, 0), 5)
+        self.assertEqual(score(-5, 0, scrbrd), 5)
 
     def test_on_the_inner_circle(self):
-        self.assertEqual(score(0, -1), 10)
+        self.assertEqual(score(0, -1, scrbrd), 10)
 
     def test_exactly_on_centre(self):
-        self.assertEqual(score(0, 0), 10)
+        self.assertEqual(score(0, 0, scrbrd), 10)
 
     def test_near_the_centre(self):
-        self.assertEqual(score(-0.1, -0.1), 10)
+        self.assertEqual(score(-0.1, -0.1, scrbrd), 10)
 
     def test_just_within_the_inner_circle(self):
-        self.assertEqual(score(0.7, 0.7), 10)
+        self.assertEqual(score(0.7, 0.7, scrbrd), 10)
 
     def test_just_outside_the_inner_circle(self):
-        self.assertEqual(score(0.8, -0.8), 5)
+        self.assertEqual(score(0.8, -0.8, scrbrd), 5)
 
     def test_just_within_the_middle_circle(self):
-        self.assertEqual(score(-3.5, 3.5), 5)
+        self.assertEqual(score(-3.5, 3.5, scrbrd), 5)
 
     def test_just_outside_the_middle_circle(self):
-        self.assertEqual(score(-3.6, -3.6), 1)
+        self.assertEqual(score(-3.6, -3.6, scrbrd), 1)
 
     def test_just_within_the_outer_circle(self):
-        self.assertEqual(score(-7.0, 7.0), 1)
+        self.assertEqual(score(-7.0, 7.0, scrbrd), 1)
 
     def test_just_outside_the_outer_circle(self):
-        self.assertEqual(score(7.1, -7.1), 0)
+        self.assertEqual(score(7.1, -7.1, scrbrd), 0)
 
     def test_asymmetric_position_between_the_inner_and_middle_circles(self):
-        self.assertEqual(score(0.5, -4), 5)
+        self.assertEqual(score(0.5, -4, scrbrd), 5)
 
 
 if __name__ == "__main__":
